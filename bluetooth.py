94e83f8a666816fe995e3c488d4ca3db40a93a00
yoshiwara <yk01crome@g>
2021-06-14 13:11:24 +0900

    test

--- bluetooth.py
+++ bluetooth.py
@@ -1,1 +1,59 @@
-#Bluetooth(子機)として認識させたい
+import os #used to all external commands
+import sys # used to exit the script
+import dbus
+import evdev
+import keymap
+import time
+from time import sleep
+
+HID_DBUS = 'org.yaptb.btkbservice'
+HID_SRVC = '/org/yaptb/btkbservice'
+debugFilePath="./Debug/"
+debugLine=''
+
+class SendKeycode:
+    """
+    Send the HID messages to the keyboard D-Bus server for specified button
+    """
+    #setup 
+    def __init__(self):
+        self.bus = dbus.SystemBus()
+        self.btkobject = self.bus.get_object(HID_DBUS,
+                                             HID_SRVC)
+        self.btk_service = dbus.Interface(self.btkobject,
+                                          HID_DBUS)
+
+    """
+    send key 
+    """
+    def popinSendKey(self, send_string):
+        targetCode = int(keymap.keytable[ send_string ])
+        targetKeys = [161, 1, 0, 0, targetCode, 0, 0, 0, 0, 0]
+        all_keys_up = [161, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        self.btk_service.send_keys(targetKeys)
+        time.sleep(0.001)
+        self.btk_service.send_keys(all_keys_up)
+
+    """
+    debug send key (save send key by txtfile)
+    """
+    def DebugPopinSendKey(self.send_string):
+        popinSendKey(self,send_string)
+
+    def writeText(self,filePath,text):
+        with open(filePath, mode='a') as f:
+            f.write(text+"\n")
+
+
+
+if __name__ == '__main__':
+    
+    #送るためのクラスを用意
+    skc = SendKeycode()
+
+    print ("Sending " + string_to_send)
+
+    skc.popinSendKey(string_to_send)
+
+    print ("Done " + string_to_send)
