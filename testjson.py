d0a227e932119dc0924c63e5e1ed1a5f6c809be5
Yoshiwara <yk01corme@gmail.com>
2021-06-17 13:53:52 +0900

    rewrite keymap.json and py

--- testjson.py
+++ testjson.py
@@ -9,15 +9,6 @@ def callKey(keymap,callID):
         if(type(callID) is not str):
             callID=str(callID)
         
-        #１文字目は1~3である
-        if(callID[0] not in ["1","2","3"] ):
-            raise Exception
-        #2文字目は4~7である
-        if(callID[1] not in ["4","5","6","7"] ):
-            raise Exception
-        #数字にすると11以上37以下である．
-        if(10<=int(callID) and int(callID)<=37 ):
-            raise Exception
     except :
         print("callKey(keymap,callID)  The callID is invalid.")
         return ""
@@ -31,5 +22,5 @@ with open("keymap.json") as jsonData:
 
  
 #serach "1_4" out ->"k"
-print(callKey(keymap,"04"))
+print(callKey(keymap,"5"))
 
